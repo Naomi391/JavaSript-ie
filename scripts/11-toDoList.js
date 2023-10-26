a0755fccfa877aa8063d860548567075d6a11587
mogi <nyabokenaomi001@gmail.com>
2023-10-27 00:45:50 +0300

    ToDo-List part One

--- scripts/11-toDoList.js
+++ scripts/11-toDoList.js
@@ -4,10 +4,9 @@ function addToDo() {
   const inputElement = document.querySelector(".js-the-list");
   const name = inputElement.value;
 
-  console.log(name);
-
   toDo.push(name);
   console.log(toDo);
 
   inputElement.value = "";
+  
 }
