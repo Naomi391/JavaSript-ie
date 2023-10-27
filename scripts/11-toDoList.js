987e76c61aebae2a9517b2a104bbbbefe20354e3
mogi <nyabokenaomi001@gmail.com>
2023-10-28 01:15:52 +0300

    Creating a toDo List and using accumulator pattern

--- scripts/11-toDoList.js
+++ scripts/11-toDoList.js
@@ -1,4 +1,21 @@
-const toDo = [];
+const toDo = [""];
+
+renderList();
+
+function renderList() {
+  let htmlActivities = "";
+
+  for (let i = 0; i < toDo.length; i++) {
+    const activities = toDo[i];
+    const html = `<p>${activities}</p>`;
+
+    htmlActivities += html;
+  }
+
+  console.log(htmlActivities);
+
+  document.querySelector(".js-to-do-list").innerHTML = htmlActivities;
+}
 
 function addToDo() {
   const inputElement = document.querySelector(".js-the-list");
@@ -8,5 +25,5 @@ function addToDo() {
   console.log(toDo);
 
   inputElement.value = "";
-  
+  renderList();
 }
