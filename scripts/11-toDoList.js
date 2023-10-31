f05fb5d6a2e86726261fcdd743c650427dbfd4a9
mogi <nyabokenaomi001@gmail.com>
2023-10-31 09:37:47 +0300

    Final toDo List project

--- scripts/11-toDoList.js
+++ scripts/11-toDoList.js
@@ -1,4 +1,13 @@
-const toDo = [""];
+const toDo = [
+  {
+    name: "make dinner",
+    dueDate: "2023 - 10 - 31",
+  },
+  {
+    name: "call Mom",
+    dueDate: "2023 - 10 - 31",
+  },
+];
 
 renderList();
 
@@ -6,13 +15,17 @@ function renderList() {
   let htmlActivities = "";
 
   for (let i = 0; i < toDo.length; i++) {
-    const activities = toDo[i];
-    const html = `<p>${activities}</p>`;
+    const activitiesObject = toDo[i];
+    const name = activitiesObject.name;
+    const dueDate = activitiesObject.dueDate;
+    const html = `<div>${name}</div> <div>${dueDate}</div> 
+    <button onclick="
+    toDo.splice(${i}, 1);
+    renderList();
+    " class="delete-button">Delete</button> `;
 
     htmlActivities += html;
   }
-
-  console.log(htmlActivities);
 
   document.querySelector(".js-to-do-list").innerHTML = htmlActivities;
 }
@@ -21,8 +34,15 @@ function addToDo() {
   const inputElement = document.querySelector(".js-the-list");
   const name = inputElement.value;
 
-  toDo.push(name);
-  console.log(toDo);
+  const dateInputElement = document.querySelector(".js-due-date");
+  const dueDate = dateInputElement.value;
+
+  toDo.push({
+    name: name,
+    dueDate: dueDate,
+    //name,
+    //dueDate
+  });
 
   inputElement.value = "";
   renderList();
